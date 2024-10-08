e14402f50d21392a17f987b3826f0d50438412e2
jaemin2943 <92981039+jaemin2943@users.noreply.github.com>
2024-10-08 13:43:59 -0400

    minigame update with sound

--- UnityProj/Assets/Scripts/Minigame1/GameController.cs
+++ UnityProj/Assets/Scripts/Minigame1/GameController.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject[] squares;
+    public AudioClip[] sounds;
     private List<int> randomSequence;
     private int currentStep;
     private bool pressable;
+    private AudioSource audioSource;
+    private float soundPlayTime = 1f;
+    private float soundTimer = 0f;
+    private bool isPlayingSound = false;
+    private int squareNum = 9;
+    private int sequenceNum = 5;
 
     void Start()
     {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        pressable = false;
+        Invoke("startLate", 1f);
+    }
+
+    void startLate()
+    {
         GenerateRandomSequence();
         StartCoroutine(SquareSequence());
+    }
+
+    void Update()
+    {
+        if (isPlayingSound)
+        {
+            soundTimer -= Time.deltaTime;
+
+            if (soundTimer <= 0f)
+            {
+                audioSource.Stop();
+                isPlayingSound = false;
+            }
+        }
     }
 
     void GenerateRandomSequence()
@@ -20,7 +48,7 @@ public class GameController : MonoBehaviour
         randomSequence = new List<int>();
         List<int> availableIndices = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 
-        while (availableIndices.Count > 0)
+        while (availableIndices.Count > squareNum - sequenceNum)
         {
             int randomIndex = Random.Range(0, availableIndices.Count);
             randomSequence.Add(availableIndices[randomIndex]);
@@ -32,8 +60,6 @@ public class GameController : MonoBehaviour
 
     IEnumerator SquareSequence()
     {
-        pressable = false;
-
         foreach (int index in randomSequence)
         {
             yield return StartCoroutine(PressSquareEffect(index));
@@ -51,8 +77,10 @@ public class GameController : MonoBehaviour
 
         color.a = 0.5f;
         renderer.color = color;
-
+        PlaySound(index);
         yield return new WaitForSeconds(1f);
+        color.a = 1f;
+        renderer.color = color;
     }
 
     public void LightUpAllSquares()
@@ -77,7 +105,7 @@ public class GameController : MonoBehaviour
                 Color color = renderer.color;
                 color.a = 0.5f;
                 renderer.color = color;
-
+                PlaySound(squareIndex);
                 Debug.Log("Correct");
 
                 if (currentStep == randomSequence.Count)
@@ -87,12 +115,21 @@ public class GameController : MonoBehaviour
             }
             else
             {
+                pressable = false;
                 Debug.Log("Incorrect");
                 currentStep = 0;
                 Invoke("LightUpAllSquares", 1f);
                 Invoke("Restart", 1f);
             }
         }
+    }
+
+    private void PlaySound(int index)
+    {
+        audioSource.clip = sounds[index];
+        audioSource.Play();
+        isPlayingSound = true;
+        soundTimer = soundPlayTime;
     }
 
     public void Restart()
